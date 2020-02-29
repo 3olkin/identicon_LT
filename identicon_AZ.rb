17b4c32917c28d5465d563bcead09c1a72405ac2
Z4RX <iamz4rx@gmail.com>
2020-02-29 17:28:47 +0300

    Final draft

--- identicon_AZ.rb
+++ identicon_AZ.rb
@@ -1,18 +1,21 @@
 require 'zlib'
 require 'chunky_png'
 Size = 250
+Grid = 5
+DSquare = Size / Grid
 
 class Identicon
   attr_accessor :user_name, :path
 
-  def initialize(user_name, path = "C:/Programming/identicon_LT/" )
+  def initialize(user_name, path = "C:/Programming/identicon_LT" )
     @user_name = user_name
     @path = path
   end
 
   def generate
     identicon_draw(image_arr(@user_name), @color)
-    @image.save(@path+"identicon_"+@id+".png")
+    @image.save(@path+"/identicon_"+@id+".png")
+    puts 'yes'
   end
 
   private
@@ -21,9 +24,9 @@ class Identicon
       userhash = Zlib::crc32(username.downcase)
       @id = userhash.to_s(16)
       rows = []
-      5.times do
+      Grid.times do
         rows.push(userhash & 31)
-        userhash = (userhash >> 5)
+        userhash = (userhash >> Grid)
       end
       @color = color_convert(userhash)
       rows
@@ -33,7 +36,7 @@ class Identicon
       r = value % 4 * 63
       g = value / 4 % 4 * 63
       b = value / 8 % 8 * 31
-      ChunkyPNG::Color.rgb(r,g,b)
+      ChunkyPNG::Color.rgb(r, g, b)
     end
 
     def identicon_draw(rows, color)
@@ -41,7 +44,7 @@ class Identicon
       rows.each_with_index do |y_item, y_index|
         bit_row(y_item).each_with_index do |x_item, x_index|
           if x_item == 1
-            png.rect(x_index*50, y_index*50, (x_index+1)*50, (y_index+1)*50, color, color )
+            png.rect(x_index * DSquare, y_index * DSquare, (x_index + 1) * DSquare, (y_index + 1) * DSquare, color, color )
           end
         end
       end
@@ -50,14 +53,13 @@ class Identicon
 
     def bit_row(num)
       bitrow = []
-      5.times do
+      Grid.times do
         bitrow.push(num % 2)
         num = (num >> 1)
       end
       bitrow
     end
-
-    def save(path)
-      @image.save(path)
-    end
 end
+
+f = Identicon.new('test', "C:/Programming/identicon_LT/Examples")
+f.generate
